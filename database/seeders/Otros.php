b6b87471da8fcca73a06d790cc109fc9e77896ef
CristianG456 <cristiancronos123@gmail.com>
2026-02-02 17:23:51 -0500

    cambios seeder

--- database/seeders/Otros.php
+++ database/seeders/Otros.php
@@ -97,9 +97,19 @@ class Otros extends Seeder
         // }
 
         $arl = [
-        'Colpatria',
-        'Sura',
-        'Bólivar',
+        // ARL con mayor cobertura nacional
+        'ARL SURA',
+        'ARL Positiva',
+        'ARL Colmena',
+
+        // ARL privadas con alta presencia
+        'ARL AXA Colpatria',
+        'ARL Bolívar',
+        'ARL Allianz',
+
+        // ARL con cobertura más focalizada
+        'ARL La Equidad',
+        'ARL Seguros de Vida Alfa',
         
         ];
 
@@ -112,9 +122,8 @@ class Otros extends Seeder
         }
 
         $forma_pago = [
-        'Mensual',
-        'Quincenal',
-        'Semanal',
+        'Contado',
+        'Crédito',
         
         ];
 
@@ -127,9 +136,84 @@ class Otros extends Seeder
         }
 
         $metodo_pago = [
-        'Transferencia',
-        'Cheque',
-        'Efectivo',
+        '1 - Instrumento no definido',
+        '2 - Crédito ACH',
+        '3 - Débito ACH',
+        '4 - Reversión débito de demanda ACH',
+        '5 - Reversión crédito de demanda ACH',
+        '6 - Crédito de demanda ACH',
+        '7 - Débito de demanda ACH',
+        '8 - Mantener',
+        '9 - Clearing Nacional o Regional',
+        '10 - Efectivo',
+        '11 - Reversión Crédito Ahorro',
+        '12 - Reversión Débito Ahorro',
+        '13 - Crédito Ahorro',
+        '14 - Débito Ahorro',
+        '15 - Bookentry Crédito',
+        '16 - Bookentry Débito',
+        '17 - Concentración de la demanda en efectivo / Desembolso Crédito (CCD)',
+        '18 - Concentración de la demanda en efectivo / Desembolso Débito (CCD)',
+        '19 - Crédito Pago negocio corporativo (CTP)',
+        '20 - Cheque',
+        '21 - Proyecto bancario',
+        '22 - Proyecto bancario certificado',
+        '23 - Cheque bancario',
+        '24 - Nota cambiaria esperando aceptación',
+        '25 - Cheque certificado',
+        '26 - Cheque Local',
+        '27 - Débito Pago Negocio Corporativo (CTP)',
+        '28 - Crédito Negocio Intercambio Corporativo (CTX)',
+        '29 - Débito Negocio Intercambio Corporativo (CTX)',
+        '30 - Transferencia Crédito',
+        '31 - Transferencia Débito',
+        '32 - Concentración Efectivo / Desembolso Crédito plus (CCD+)',
+        '33 - Concentración Efectivo / Desembolso Débito plus (CCD+)',
+        '34 - Pago y depósito pre acordado (PPD)',
+        '35 - Concentración efectivo ahorros / Desembolso Crédito (CCD)',
+        '36 - Concentración efectivo ahorros / Desembolso Débito (CCD)',
+        '37 - Pago Negocio Corporativo Ahorros Crédito (CTP)',
+        '38 - Pago Negocio Corporativo Ahorros Débito (CTP)',
+        '39 - Crédito Negocio Intercambio Corporativo (CTX)',
+        '40 - Débito Negocio Intercambio Corporativo (CTX)',
+        '41 - Concentración efectivo / Desembolso Crédito plus (CCD+)',
+        '42 - Consignación bancaria',
+        '43 - Concentración efectivo / Desembolso Débito plus (CCD+)',
+        '44 - Nota cambiaria',
+        '45 - Transferencia Crédito Bancario',
+        '46 - Transferencia Débito Interbancario',
+        '47 - Transferencia Débito Bancaria',
+        '48 - Tarjeta Crédito',
+        '49 - Tarjeta Débito',
+        '50 - Postgiro',
+        '51 - Telex estándar bancario francés',
+        '52 - Pago comercial urgente',
+        '53 - Pago Tesorería Urgente',
+        '60 - Nota promisoria',
+        '61 - Nota promisoria firmada por el acreedor',
+        '62 - Nota promisoria firmada por el acreedor, avalada por el banco',
+        '63 - Nota promisoria firmada por el acreedor, avalada por un tercero',
+        '64 - Nota promisoria firmada por el banco',
+        '65 - Nota promisoria firmada por un banco avalada por otro banco',
+        '66 - Nota promisoria firmada',
+        '67 - Nota promisoria firmada por un tercero avalada por un banco',
+        '70 - Retiro de nota por el acreedor',
+        '71 - Bonos',
+        '72 - Vales',
+        '74 - Retiro de nota por el acreedor sobre un banco',
+        '75 - Retiro de nota por el acreedor, avalada por otro banco',
+        '76 - Retiro de nota por el acreedor, sobre un banco avalada por un tercero',
+        '77 - Retiro de una nota por el acreedor sobre un tercero',
+        '78 - Retiro de una nota por el acreedor sobre un tercero avalada por un banco',
+        '91 - Nota bancaria transferible',
+        '92 - Cheque local transferible',
+        '93 - Giro referenciado',
+        '94 - Giro urgente',
+        '95 - Giro formato abierto',
+        '96 - Método de pago solicitado no usado',
+        '97 - Clearing entre partners',
+        '98 - Cuentas de Ahorro de Trámite Simplificado (CATS) (Nequi, Daviplata, etc)',
+        'ZZZ - Acuerdo mutuo',
         
         
         ];
@@ -142,8 +226,12 @@ class Otros extends Seeder
             ]);
         }
         $tipo_cuenta = [
-        'Ahorros',
-        'Corriente',
+        // Tipos de cuenta bancarios
+        'Cuenta de Ahorros',
+        'Cuenta Corriente',
+
+        // Cuentas simplificadas (válidas según regulación financiera)
+        'Cuenta de Ahorro de Trámite Simplificado (CATS)',
         
         
         ];
@@ -157,11 +245,41 @@ class Otros extends Seeder
         }
 
         $eps = [
-        'Sura',
-        'Sanitas',
-        'Coomeva',
+         // EPS con mayor cobertura nacional (Contributivo + Subsidiado)
+        'Nueva EPS',
+        'Coosalud',
+        'Mutual Ser',
+        'Salud Mía',
+
+        // EPS régimen contributivo (alta presencia)
+        'EPS Sura',
+        'EPS Sanitas',
         'Salud Total',
+        'Famisanar',
         'Compensar',
+        'Aliansalud',
+        'Servicio Occidental de Salud (SOS)',
+        'Comfenalco Valle',
+        'Empresas Públicas de Medellín (EPM)',
+        'Fondo de Pasivo Social de Ferrocarriles Nacionales de Colombia',
+
+        // EPS régimen subsidiado (cobertura regional y poblacional)
+        'Emssanar',
+        'Capital Salud',
+        'Savia Salud',
+        'Asmet Salud',
+        'Cajacopi Atlántico',
+        'Capresoca',
+        'EPS Familiar de Colombia',
+        'Comfachocó',
+        'Comfaoriente',
+
+        // EPS Indígenas (EPSI)
+        'Asociación Indígena del Cauca EPSI',
+        'Anas Wayuu EPSI',
+        'Dusakawi EPSI',
+        'Mallamas EPSI',
+        'Pijaos Salud EPSI',
         
         
         
@@ -176,10 +294,14 @@ class Otros extends Seeder
         }
 
         $afp = [
+        // AFP privadas (mayor número de afiliados)
+        'Porvenir',
         'Protección',
         'Colfondos',
-        'Porvenir',
-        'Old Mutua',
+        'Skandia',
+
+        // Régimen de Prima Media (público)
+        'Colpensiones',
         
         
         

f61902fb64ca9bd9738eff33ea862a8386d693fb
JoanFlorian <florezk546@gmail.com>
2026-03-27 12:53:52 -0500

    Limpieza de archivos de diagnostico y temporales

--- dump_modules.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$modules = DB::table('permissions')->distinct()->pluck('module')->toArray();
-file_put_contents('modules_dump.txt', implode("\n", $modules));
-echo "Done\n";

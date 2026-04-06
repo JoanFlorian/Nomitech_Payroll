c913053cbf1d9c8ebbbcbd2a0dfd2dfa20baac4a
JoanFlorian <florezk546@gmail.com>
2026-04-06 03:28:55 -0500

    Force SESSION_DOMAIN null to fix cookie rejection

--- config/session.php
+++ config/session.php
@@ -150,13 +150,13 @@ return [
     | Session Cookie Domain
     |--------------------------------------------------------------------------
     |
-    | This value determines the domain and subdomains the session cookie is
-    | available to. By default, the cookie will be available to the root
-    | domain and all subdomains. Typically, this shouldn't be changed.
+    | Forcing domain to null prevents cookie rejection when connecting through
+    | various proxies and alternate environment URLs like Render onrender domains.
     |
     */
 
-    'domain' => env('SESSION_DOMAIN'),
+    'domain' => null, // Ignoramos env('SESSION_DOMAIN') en duro para forzar aceptación
+
 
     /*
     |--------------------------------------------------------------------------

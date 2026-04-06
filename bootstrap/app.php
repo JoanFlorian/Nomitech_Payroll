7a2d74945238eeed6f4b5ac7d998a409af71ddad
JoanFlorian <florezk546@gmail.com>
2026-04-06 03:08:44 -0500

    Fix sessions table user_id type and add proxy trust

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '/stripe/webhook',
         ]);
+        
+        $middleware->trustProxies(at: '*');
+
 
         $middleware->alias([
             'ensure_active_license' => \App\Http\Middleware\EnsureActiveLicense::class,
